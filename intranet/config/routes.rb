39722eb778afeeb265d5666ab53416b1da7256e9
emerson2017 <emersondima@hotmail.com>
2017-10-10 17:36:08 -0300

    site - menu

--- intranet/config/routes.rb
+++ intranet/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :pessoas
+  resources :site
   devise_for :users
+  resources :products
+
   
   get 'welcome/index'
-
   root 'welcome#index'
 
-  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- intranet/db/schema.rb
+++ intranet/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009175512) do
+ActiveRecord::Schema.define(version: 20171010180224) do
 
   create_table "pessoas", force: :cascade do |t|
     t.string "nome"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20171009175512) do
   create_table "products", force: :cascade do |t|
     t.string "description"
     t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sites", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
