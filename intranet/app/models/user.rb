0771bbbd5391f8a81f0d2ed7fa6f7446c76943ae
emerson2017 <emersondima@hotmail.com>
2017-11-08 15:54:09 -0300

    ...

--- intranet/app/models/user.rb
+++ intranet/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 has_many :mural_atividades
 has_many :minhas_atividades
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
