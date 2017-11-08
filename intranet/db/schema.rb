53fdbb116cf92aa82e4bb71a56fd6b92658d1325
root <root@localhost.localdomain>
2017-11-08 00:35:25 -0200

    adicionando setor....

--- intranet/db/schema.rb
+++ intranet/db/schema.rb
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20171107183517) do
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
