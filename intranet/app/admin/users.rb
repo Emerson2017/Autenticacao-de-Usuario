caa4b4c81714a0cd5188e145142a69907e9fb014
emerson2017 <emersondima@hotmail.com>
2017-11-14 15:53:34 -0300

    prosseguindo...

--- intranet/app/admin/users.rb
+++ intranet/app/admin/users.rb
@@ -1,12 +1,15 @@
 ActiveAdmin.register User do
-	  permit_params [:email, :password, :password_confirmation, :username] 
+	  permit_params [:email, :password, :password_confirmation, :username, :Setor, :admin] 
 
  form do |f|
       f.inputs "User" do
-        f.input :email
-        f.input :password
-        f.input :password_confirmation
-        f.input :username
+      	f.input :username, :label => 'Nome de Usuário'
+        f.input :email, :label => 'Email'
+        f.input :password, :label => 'Senha'
+        f.input :password_confirmation, :label => 'Confirmar Senha'
+        f.input :admin, :label => 'Administrador ?'
+        f.select :Setor, :selecione =>  ['Administrador', 'RH', 'Colaborador', 'Comercial', 
+        	'Técnico', 'Financeiro', 'Diretoria' ]
       end
       f.actions
     end
--- intranet/db/seeds.rb
+++ intranet/db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-AdminUser.create!(email: 'admin@quanta.com', password: '123456', password_confirmation:'123456')
+AdminUser.create!(:email => 'admin@quanta.com', :password => '123456', :password_confirmation=>'123456')
